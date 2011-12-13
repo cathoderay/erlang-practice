263c511f4b52a671bbfe614f4ea9240d55216249
Ronald Kaiser <raios.catodicos@gmail.com>
2011-12-13 21:09:07 -0200

    some practicing

---
+++ erlang-practice.erl
@@ -0,0 +1,44 @@
+% File: erlang-practice.erl 
+% Goal: spot erlang characterists
+
+
+%arbitrary precision
+100000000000000000000000000000*2000000
+
+%single assignment - like algebra
+X = 1. % . (dot) force evalutation in interpreter
+
+%atoms
+hello.
+
+%tuples.
+Point = {point, 10, 45}. %point first, good practice
+
+%accessing tuple values - via pattern matching
+{point, X, Y} = Point.
+X.
+10
+Y.
+45
+
+%anonymous variable
+{A, _, B} = {1, 2, 3}.
+
+
+%lists
+List = [4, 2, {foo, bar}].
+
+
+%extracting head/tail from lists
+[H|T] = [1, 2, 3].
+H
+1
+T.
+[2,3]
+
+%strings - just a list of integers - dependent on character set
+[83, 117, 114, 112, 114, 105, 115, 101].
+"Surprise"
+
+
+
